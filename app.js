9035dd4ce5e7c7f81cd4868c4bb13685cdd61264
Jonathan Cordero <cryptcur00101@protonmail.com>
2020-01-30 11:12:37 -0500

    updated

--- app.js
+++ app.js
@@ -16,10 +16,31 @@ const renderUsers = usersArr => {
     .join("");
   userList.innerHTML = html;
 };
+const renderLinks = (arrOfUsers, count) => {
+  let html = [];
+  let currentPage = Math.ceil(count / 50);
+  for (let i = 0; i <= currentPage; i++) {
+    console.log(currentPage);
+
+    html.push(`
+            <a href="#${i - 1}">${i}</a>
+        `);
+  }
+  // html.join('')
+  header.innerHTML = html.join("");
+};
 fetch("https://acme-users-api-rev.herokuapp.com/api/users")
   .then(response => response.json())
   .then(data => {
     const { users, count } = data;
-    renderUsers(data.users);
-    console.log(users)
+    renderUsers(users);
+    renderLinks(users, count);
+    console.log(users);
   });
+window.addEventListener("hashchange", ev => {
+  const id = window.location.hash.slice(1);
+  console.log(id);
+  fetch(`https://acme-users-api-rev.herokuapp.com/api/users/${id}`)
+    .then(response => response.json())
+    .then(data => renderUsers(data.users));
+});
